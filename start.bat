30693d8da5f0375bedd04073f9cfb5cf54885d1e
Bhesh Sejawal <bsejawal@gmail.com>
2020-05-03 13:24:47 -0500

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,3 +1,5 @@
+CD /D "C:\s3sync\
+call mvn clean package
 for /f "tokens=1" %%i in ('jps -m') do ( taskkill /F /PID %%i )
 CD /D "C:\s3sync\target\"
 start /min java -jar -Xmx4096M s3sync.jar >> log\s3sync-error.log
